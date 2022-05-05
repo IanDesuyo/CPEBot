3b19114536671f60883267577da76c9d45d9d392
IanDesuyo <59338745+IanDesuyo@users.noreply.github.com>
2022-05-05 11:44:34 +0800

    use more while loop to handle network error

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+from asyncore import write
 from datetime import datetime
+import logging
 from requests import Session
 from bs4 import BeautifulSoup
 from time import sleep
-from base64 import b64decode
+from logging import getLogger
 
 
 class CPEBot:
     def __init__(self, username: str, password: str):
+        self.logger = getLogger(__name__)
         self.username = username
         self.password = password
         self.session = Session()
@@ -15,8 +18,6 @@ class CPEBot:
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
             }
         )
-        self.isLogin = False
-        self.isSuccess = False
         self.user_data = {}
 
     def login(self):
@@ -27,61 +28,83 @@ class CPEBot:
             "pw": self.password,
         }
 
-        while not self.isLogin:
+        while True:
             try:
-                r = self.session.get("https://cpe.cse.nsysu.edu.tw/cpe/", timeout=10)
-            except TimeoutError:
+                res = self.session.get("https://cpe.cse.nsysu.edu.tw/cpe/", timeout=600)
+                self.logger.info(f"[Login] Get {res.status_code}")
+            except:
                 continue
 
-            soup = BeautifulSoup(r.text, "html.parser")
-            captcha = soup.select_one("#new_captcha img").get("src")
+            break
 
+        soup = BeautifulSoup(res.text, "html.parser")
+        captcha = soup.select_one("#new_captcha img").get("src")
+
+        # fetch captcha
+        while True:
             try:
-                r1 = self.session.get(f"https://cpe.cse.nsysu.edu.tw/cpe/{captcha}")
-            except TimeoutError:
+                resCaptcha = self.session.get(
+                    f"https://cpe.cse.nsysu.edu.tw/cpe/{captcha}", timeout=600
+                )
+                self.logger.info(f"[Login] Get Captcha {res.status_code}")
+            except:
                 continue
 
-            with open("captcha.jpg", "wb+") as f:
-                f.write(r1.content)
+            break
 
-            captcha_code = input("驗證碼: ")
+        with open("captcha.jpg", "wb+") as f:
+            f.write(resCaptcha.content)
 
+        captcha_code = input("驗證碼: ")
+
+        while True:
             try:
-                r = self.session.post(
+                res = self.session.post(
                     "https://cpe.cse.nsysu.edu.tw/cpe/",
                     data={**payload, "captcha": captcha_code},
                     allow_redirects=False,
+                    timeout=600,
                 )
-            except TimeoutError:
+                self.logger.info(f"[Login] Post Data {res.status_code}")
+            except:
                 continue
 
-            soup = BeautifulSoup(r.text, "html.parser")
+            break
 
-            if r.is_redirect:  # login success
-                self.isLogin = True
-                r = self.session.get("https://cpe.cse.nsysu.edu.tw/cpe/newest")
-                soup = BeautifulSoup(r.text, "html.parser")
+        if res.is_redirect:  # login success
+            while True:
+                try:
+                    res = self.session.get(
+                        "https://cpe.cse.nsysu.edu.tw/cpe/newest", timeout=600
+                    )
+                    self.logger.info(f"[Login] Get Newest {res.status_code}")
+                except:
+                    continue
 
-                self.user_data["myGrade"] = soup.select_one("select[name='myGrade'] option[selected]").get("value")
-                self.user_data["myDepartment"] = soup.select_one("input[name='myDepartment']").get("value")
-                self.user_data["mySchoolID"] = soup.select_one("select[name='mySchoolID'] option[selected]").get(
-                    "value"
-                )
-                self.user_data["optionsRadios1"] = 1 if soup.select_one("#optionsRadios1[checked]") else 0
+                break
 
-                success_alert = soup.select_one(".alert-success")
-                if success_alert:
-                    print("已報名過了!", success_alert.text.strip())
-                    return
+            soup = BeautifulSoup(res.text, "html.parser")
+            self.user_data["myGrade"] = soup.select_one(
+                "select[name='myGrade'] option[selected]"
+            ).get("value")
+            self.user_data["myDepartment"] = soup.select_one(
+                "input[name='myDepartment']"
+            ).get("value")
+            self.user_data["mySchoolID"] = soup.select_one(
+                "select[name='mySchoolID'] option[selected]"
+            ).get("value")
+            self.user_data["optionsRadios1"] = (
+                1 if soup.select_one("#optionsRadios1[checked]") else 0
+            )
 
-            else:
-                for error in soup.select(".text-error strong"):
-                    print(error.text.strip())
+        else:
+            for err in soup.select(".text-error strong"):
+                self.logger.error(err)
 
     def apply(self, school_id: int):
-        while not self.isSuccess:
+        while True:
             try:
-                r = self.session.post(
+                res = self.session.post(
                     "https://cpe.cse.nsysu.edu.tw/cpe/newest",
                     data={
                         **self.user_data,
@@ -89,29 +112,38 @@ class CPEBot:
                         "site": school_id,
                         "yesExam": "報名",
                     },
-                    timeout=3,
+                    timeout=600,
                 )
-            except TimeoutError:
+                self.logger.info(f"[Login] PostData {res.status_code}")
+            except:
                 continue
 
-            if r.is_permanent_redirect:
+            if res.is_permanent_redirect:
                 self.login()
                 continue
 
-            soup = BeautifulSoup(r.text, "html.parser")
+            soup = BeautifulSoup(res.text, "html.parser")
 
             success_alert = soup.select_one(".alert-success")
             error_alert = soup.select_one(".alert-error")
+
             if success_alert:
-                self.isSuccess = True
-                print(success_alert.text.strip())
+                self.logger.info(f"[Apply] {success_alert.text.strip()}")
+                break
 
             if error_alert:
-                print(error_alert.text.strip())
+                self.logger.error(f"[Apply] {error_alert.text.strip()}")
 
             sleep(3)
 
 
-bot = CPEBot("username", "password")
-bot.login()
-bot.apply(33)
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    bot = CPEBot("username", "password")
+    bot.login()
+    bot.apply(33)
